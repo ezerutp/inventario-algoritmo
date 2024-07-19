58d09341bbba5e421e8bb4b7ef7d9c9a01a0619b
ezerutp <u22329821@utp.edu.pe>
2024-07-19 01:52:38 -0500

    parametro para listas

--- inventario/InventarioAlgoritmo/src/appinventario/controllers/HomeController.java
+++ inventario/InventarioAlgoritmo/src/appinventario/controllers/HomeController.java
@@ -54,15 +54,15 @@ public class HomeController {
         return new UsuarioTableModel(admcontrol.obtenerTodosUsuarios());
     }
     
-    public RConsumoTableModel listaConsumos(){
+    public RConsumoTableModel listaConsumos(int filas){
         BiPredicate<Consumo, Consumo> predicado = (c1, c2) -> c1.getId() < c2.getId();
-        return new RConsumoTableModel(conscontrol.obtenerTodosConsumosOrdenados(predicado));
+        return new RConsumoTableModel(conscontrol.obtenerTodosConsumosOrdenados(predicado), filas);
     }
 
-    public RSuministroTableModel listaSuministros(){
+    public RSuministroTableModel listaSuministros(int filas){
         //criterio de ordenamiento (ordenar y mostrar los ultimos 5 ingresos)
         BiPredicate<Suministro, Suministro> predicado = (s1, s2) -> s1.getId() < s2.getId();
-        return new RSuministroTableModel(sumicontrol.obtenerTodosSuministrosOrdenados(predicado));
+        return new RSuministroTableModel(sumicontrol.obtenerTodosSuministrosOrdenados(predicado), filas);
     }
 
     public ProductoTableModel listaProductos(){
